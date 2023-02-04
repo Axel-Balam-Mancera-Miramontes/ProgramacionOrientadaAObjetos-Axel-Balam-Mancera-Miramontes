06440734174437e7649e9909dea699775249028d
Axel-Balam-Mancera-Miramontes <manceraaxel@hotmail.com>
2023-02-03 19:04:25 -0600

    Branch: reto2 Commit 2

--- src/edu/axel/balam/mancera/miramontes/reto2/programa2/process/TicketMachine.java
+++ src/edu/axel/balam/mancera/miramontes/reto2/programa2/process/TicketMachine.java
@@ -2,25 +2,21 @@ package edu.axel.balam.mancera.miramontes.reto2.programa2.process;
 
 public class TicketMachine {
     /**
-     * Method to print ticket price
-     * @param ticketType: ticket type
+     * Constant variables for ticket prices
      */
-    public static void showTicketPrice(char ticketType){
-        switch(ticketType){
-            case 'N':
-            case 'B':
-                System.out.println("El costo por el boleto es $70");
-                break;
-            case 'A':
-                System.out.println("El costo por el boleto es $100");
-                break;
-            case 'I':
-                System.out.println("El costo por el boleto es $50");
-                break;
-            default:
-                System.out.println("Tipo de boleto inválido.");
-        }
+    static final float KID_TICKET_PRICE = 70.0f, ADULT_TICKET_PRICE = 100.0f, ELDER_TICKET_PRICE = 70.0f, ELDER_TICKET_PRICE_WCARD = 50.0f;
+
+    /**
+     * Method to print ticket price
+     * @param nKids: number of kid tickets
+     * @param nAdults: number of adult tickets
+     * @param nElders: number of elder tickets
+     * @param nEldersWCard: number of elder woth INAPAM tickets
+     */
+    public static float getTotal(short nKids, short nAdults, short nElders, short nEldersWCard){
+        return nKids * KID_TICKET_PRICE + nAdults * ADULT_TICKET_PRICE + nElders * ELDER_TICKET_PRICE + nEldersWCard * ELDER_TICKET_PRICE_WCARD;
     }
+
 }
 
 
--- src/edu/axel/balam/mancera/miramontes/reto2/programa2/ui/CLI.java
+++ src/edu/axel/balam/mancera/miramontes/reto2/programa2/ui/CLI.java
@@ -11,21 +11,38 @@ public class CLI {
     public static void showUserInterface(){
         Scanner input = new Scanner(System.in);
         char ticketType, option;
+        short nKids = 0, nAdults = 0, nElders = 0, getnEldersWCard = 0;
 
         System.out.println("¿Qué tipo de boleto desea comprar?" +
-                "\nN) Boleto para niño." +
-                "\nA) Boleto para adulto." +
-                "\nB) Boleto para adulto mayor" +
-                "\nI) Boleto para adulto mayor con credencial INAPAM.");
+                "\nN) Boleto para niño: $70." +
+                "\nA) Boleto para adulto: $100." +
+                "\nB) Boleto para adulto mayor: $70" +
+                "\nI) Boleto para adulto mayor con credencial INAPAM: %50.");
         do{
             System.out.print("Ingrese el tipo de boleto: ");
             ticketType = input.nextLine().charAt(0);
-            TicketMachine.showTicketPrice(ticketType);
+            switch(ticketType){
+                case 'N':
+                    ++nKids;
+                    break;
+                case 'A':
+                    ++nAdults;
+                    break;
+                case 'B':
+                    ++nElders;
+                    break;
+                case 'I':
+                    ++getnEldersWCard;
+                    break;
+                default:
+                    System.out.println("Tipo de boleto inválido.");
+            }
 
             System.out.print("¿Desea comprar otro boleto? ");
             option = input.nextLine().charAt(0);
         }while(option == 's');
 
+        System.out.println("El costo toal por los boletos comprados es de : $" + TicketMachine.getTotal(nKids, nAdults, nElders, getnEldersWCard));
         System.out.println("¡Fin del programa!");
     }
 }
