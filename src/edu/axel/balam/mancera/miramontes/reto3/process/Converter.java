693b3c983d59c803132b76e6d6a7cea7983d749e
Axel-Balam-Mancera-Miramontes <manceraaxel@hotmail.com>
2023-02-09 18:33:23 -0600

    Branch: reto3 Commit 2

--- src/edu/axel/balam/mancera/miramontes/reto3/process/Converter.java
+++ src/edu/axel/balam/mancera/miramontes/reto3/process/Converter.java
@@ -16,7 +16,15 @@ public class Converter {
      * @return the decimal's representation in the binary/octal/hexadecimal system
      */
     public static String cnvrtDecToBinOctHex(String decBaseNum, byte systemBase){
-        return Integer.toString(Integer.parseInt(decBaseNum), systemBase);
+        String strOut = "";
+        int num = Integer.parseInt(decBaseNum);
+        char[] digits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+        while(num > 0){
+            strOut = digits[num % systemBase] + strOut;
+            num /= systemBase;
+        }
+        return strOut;
     }
 
     /**
@@ -25,7 +33,15 @@ public class Converter {
      * @return the bynary/octal/hexadecimal's representation in the decimal system
      */
     public static String cnvrtBinOctHexToDec(String binOctHexBaseNum, byte systemBase){
-        return Integer.toString(Integer.parseInt(binOctHexBaseNum, systemBase));
+        String strOut = "";
+        int num = 0;
+
+        for(short i = 0; i < binOctHexBaseNum.length(); ++i){
+            System.out.println("binOctHexBaseNum.charAt(i) = " + binOctHexBaseNum.charAt(i));
+            num += (int)Math.pow(systemBase, i) * Character.getNumericValue(binOctHexBaseNum.charAt(binOctHexBaseNum.length() - i - 1));
+        }
+
+        return Integer.toString(num);
     }
 
 }
