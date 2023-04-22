760090878d8f6d5e9fe85d35224d39b7434c960e
Axel-Balam-Mancera-Miramontes <manceraaxel@hotmail.com>
2023-04-21 21:40:43 -0600

    Branch: reto11 Commit 1

--- src/edu/axel/balam/mancera/miramontes/reto11/classes/Employee.java
+++ src/edu/axel/balam/mancera/miramontes/reto11/classes/Employee.java
@@ -1,16 +1,16 @@
-package edu.axel.balam.mancera.miramontes.reto10.classes;
+package edu.axel.balam.mancera.miramontes.reto11.classes;
 
 /**
  * The Employee class includes the attributes that are necessary to represent an employee.
  * As it's an abstract class, some of its methods will be overriden by its subclasses.
  */
 public abstract class Employee {
-    public Object edad;
     private String nomina;
     private String fechaContratacion;
     private double salario;
     public String nombre;
     public String email;
+    public short edad;
 
     /**
      * trabajar: this method will be overriden by Employee's subclasses.
